b2e1cc2aaec8d63d8da25c547aeb476b5379a343
eoleon <1genya1@rambler.ru>
2016-06-01 12:57:21 +0300

    Fix build.gradle and fix safaritest

--- tests/test/SafariDriverTests.java
+++ tests/test/SafariDriverTests.java
@@ -2,18 +2,17 @@ package test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 //@RunWith(JUnit4.class)
 public class SafariDriverTests {
 
-    private WebDriver driver = new InternetExplorerDriver();
+    private WebDriver driver = new SafariDriver();
 
     @org.testng.annotations.Test(description = "Test")
     public void test() {
-        System.setProperty("webdriver.ie.driver",System.getProperty("user.home"));
         driver.get("http://www.google.com");
         driver.findElement(By.name("q")).sendKeys("webdriver");
         driver.findElement(By.name("btnG")).click();
